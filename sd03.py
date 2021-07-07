9490b844c6cc4f6c173ae7b37f7a6bca357afc61
Rodrigo Costa <svork@users.noreply.github.com>
2021-07-07 20:05:52 -0300

    Exercise 03 and Study drill 03

---
+++ sd03.py
@@ -0,0 +1,39 @@
+# Print count chickens
+print("I will now count my chickens:")
+
+# Counting animals
+print("Hens", 25.2342 + 30.05942 / 6.05942)
+print("Roosters", 100.05942 - 25.05942 * 3.575 % 4.575)
+
+# Count even more
+print("Now I will count the eggs:")
+
+# Doing some math
+print(3.5 + 2.5 + 1.5 - 5.2 + 4.2 % 2.2 - 1.2 / 4.2 + 6.2)
+
+# Booleans
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Even more math
+print(3.9 - 2.9 < 5.9 - 7.9)
+
+# Testing operators
+print("What is 3 + 2?", 3.1 + 2.4)
+print("What is 5 - 7?", 5.9 - 7.8)
+
+# It is False
+print("Oh, that's why it's False.")
+
+# More printing
+print("How about some more.")
+
+# More testing
+print("Is it greater?", 5.4 > -2.5)
+print("Is it greater or equal?", 5.9 >= -2.1)
+print("Is it less or equal?", 5.1 <= -2.4)
+
+# My own calculations
+x = 5.6 - 2.2
+y = 10.9 / 3.1 % 2.2
+print("X equals ", x)
+print("Y equals ", y)
