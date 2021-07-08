5ad0148e6520d1a759d13ab7850eaba5415dac74
Rodrigo Costa <svork@users.noreply.github.com>
2021-07-08 20:10:02 -0300

    Exercise 10 and Study drill 10

---
+++ sd10.py
@@ -0,0 +1,21 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+bell_cat = "I do a bell.\a"
+vertical_cat = "I'm vertical\v"
+feed_cat = "Form feed cat\f"
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(bell_cat)
+print(vertical_cat)
+print(feed_cat)
