23115b840a4ef0a32c66f0f5c2752393f8849166
Rodrigo Costa <svork@users.noreply.github.com>
2021-07-10 14:24:17 -0300

    Exercise 13 and Study drill 13

---
+++ sd13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("Your first variable is: ", first)
+print("Your second variable is: ", second)
+print("Your third variable is: ", third)
+last = input("Type the last stuff you want: ")
+print(last)
