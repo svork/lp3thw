9af3da18bec517f2d9556c169b47ff6c0d35ef1b
Rodrigo Costa <svork@users.noreply.github.com>
2021-08-06 19:30:17 -0300

    Exercise 34

---
+++ ex34.py
@@ -0,0 +1,16 @@
+# animals list
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+# function to print their name and position
+def print_animal(animal_list, index):
+    print(f"The animal at index {index} is a {animal_list[index]}.")
+
+# printing animals names
+print_animal(animals, 1)
+print_animal(animals, 2)
+print_animal(animals, 0)
+print_animal(animals, 3)
+print_animal(animals, 4)
+print_animal(animals, 2)
+print_animal(animals, 5)
+print_animal(animals, 4)
