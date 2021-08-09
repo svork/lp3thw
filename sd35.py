4e2437b2d720fa4c54c1ca3980b8b2829302e657
Rodrigo Costa <svork@users.noreply.github.com>
2021-08-09 18:27:24 -0300

    Study drill 35

--- sd35.py
+++ sd35.py
@@ -4,10 +4,12 @@ def gold_room():
     print("This room is full of gold. How much do you take?")
 
     choice = input("> ")
-    if "0" in choice or "1" in choice:
-        how_much = int(choice)
+
+    if choice:
+        how_much = float(choice)
     else:
         dead("Man, learn to type a number.")
+        exit(1)
 
     if how_much < 50:
         print("Nice, you are not greedy, you win")
